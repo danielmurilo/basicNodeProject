9b26672c3bba992fe7805a875c9e138268f569ed
danielmurilo <danielmurilo@yahoo.com.br>
2023-02-23 09:37:32 -0300

    header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Header from './components/Header/Header'
 import ReadAll from './components/ReadAll/ReadAll'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <ReadAll />
     </div>
   )
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return <div>
+        <img src="https://oceanbrasil.com/assets/logo.svg" alt="Logo da Ocean" title="Logo da Ocean"/>
+        <a href="#">Home</a>
+        <a href="#">Criar</a>
+    </div>
+}
+
+export default Header
