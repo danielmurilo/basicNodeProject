292b30ae3c92c9486b4216b6537f50aba095a012
danielmurilo <danielmurilo@yahoo.com.br>
2023-02-23 10:56:04 -0300

    tags added

--- frontend/src/components/cards/Card.jsx
+++ frontend/src/components/cards/Card.jsx
@@ -1,11 +1,22 @@
 import "./Card.css"
 
-function Card(props) {
+function Card(props){
     const item = props.item
+    const tags = item.tags
     return(
         <div className="card">
             <h1>{item.nome}</h1>
-            <img src={item.imagemUrl} alt="" />
+
+            {/* Conditional renderization for tags element */}
+            {tags && (
+                <div className="tag-wrapper">
+                    {tags.map( function(tag, index){
+                        return <div className="tag" key={`tag-${index}`}>{tag}</div>
+                    })}
+                </div>
+            )}            
+            
+            <img src={item.imagemUrl} alt="" />            
         </div>
     )
 }
