45c0a3aa6a7f9a5a929a335b844a0c0143fdce65
danielmurilo <danielmurilo@yahoo.com.br>
2023-02-24 13:04:54 -0300

    ReadByID implementation

--- frontend/src/components/ReadById/ReadById.jsx
+++ frontend/src/components/ReadById/ReadById.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Card from '../cards/Card'
 
 function ReadById() {
-  return (
-    <div>ReadById</div>
-  )
+    const [item, setItem] = useState([])
+    const id = '63e656672cffe544f592081e'
+
+    async function toRequest(){
+        const url = 'http://localhost:3000/itens/' + id
+        const response = await fetch(url)
+        const data = await response.json()
+        setItem(data)
+    }
+
+    useEffect(function(){ toRequest() }, [])
+    
+
+    return (
+        <div className='ReadById'><Card item={item} /></div>
+    )
 }
 
 export default ReadById
